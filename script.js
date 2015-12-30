1e9cb5b879229312e592a5f038a3faac876e7322
Alex Smith <iqwz@ya.ru>
2015-12-30 19:10:50 +0900

    init commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+var ReactWeather = React.createClass({
+  getInitialState: function() {
+    return { 
+      userLocation: 'London'
+    };
+  },
+
+  componentDidMount: function() {
+    var self = this;
+    self._getCityWeather('London', function(result) {
+      console.log(result);
+      if ( self.isMounted() ) {
+        self.setState({
+          temp: result.coord.lat
+        });
+      }
+    });
+  },
+
+  _getCityWeather: function(city, cb) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=2de143494c0b295cca9337e1e96b00e0'
+    $.get(url, cb);
+  },
+  
+  render: function () {
+    var state = this.state;
+    console.log(state);
+    return (
+      <h4>AAAA: {state.temp}</h4>
+    );
+  }
+});
+
+ReactDOM.render(
+  <ReactWeather />,
+  document.getElementById('ReactWeather')
+);
+
+/*
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=2de143494c0b295cca9337e1e96b00e0'
+$.get(url, function(data) {
+  console.log(data);
+});
+*/
+//Иконки: http://openweathermap.org/img/w/10d.png

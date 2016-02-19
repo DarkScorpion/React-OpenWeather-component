72602e4f40ba8adae5f79955252969a0ea924197
Alex Smith <iqwz@ya.ru>
2016-02-19 13:32:51 +0900

    use reactDOM for render

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 'use strict';
 var React = require('react');
+var ReactDOM = require('react-dom');
 var ReactWeather = require('./ReactWeather.js');
 
-React.render(
+ReactDOM.render(
   <ReactWeather appID='44db6a862fba0b067b1930da0d769e98' />,
   document.getElementById('ReactWeather')
 );
